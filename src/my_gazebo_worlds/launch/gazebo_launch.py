7e6bb23123df7fd513745d6b66b77465054d95c9
root <root@BEARBOT9000>
2024-06-05 05:37:22 -0700

    updated code

--- src/my_gazebo_worlds/launch/gazebo_launch.py
+++ src/my_gazebo_worlds/launch/gazebo_launch.py
@@ -1,80 +1,73 @@
-import os
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
-    robot_description_path = os.path.join(
-        get_package_share_directory('my_gazebo_worlds'),
-        'urdf',
-        'robot_arm.urdf'
+    share_dir = get_package_share_directory('arm_bot_description')
+
+    xacro_file = os.path.join(share_dir, 'urdf', 'arm_bot.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_urdf}
+        ]
     )
-    controller_config_path = os.path.join(
-        get_package_share_directory('my_gazebo_worlds'),
-        'config',
-        'robot_controllers.yaml'
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzserver.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'pause': 'true'
+        }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzclient.launch.py'
+            ])
+        ])
+    )
+
+    urdf_spawn_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'arm_bot',
+            '-topic', 'robot_description'
+        ],
+        output='screen'
     )
 
     return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': True}, {'robot_description': open(robot_description_path).read()}]
-        ),
-        Node(
-            package='controller_manager',
-            executable='ros2_control_node',
-            parameters=[{'robot_description': open(robot_description_path).read()}, controller_config_path],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['joint_state_controller'],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['Revolute_9_position_controller'],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['Revolute_10_position_controller'],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['Revolute_11_position_controller'],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['Revolute_12_position_controller'],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['Revolute_13_position_controller'],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['Slider_14_position_controller'],
-            output='screen'
-        ),
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['Slider_15_position_controller'],
-            output='screen'
-        )
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        gazebo_server,
+        gazebo_client,
+        urdf_spawn_node,
     ])
-
